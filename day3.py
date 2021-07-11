1a55d123904737c3d997caa51bc31d9425043f64
Vladas Verkelis <vverkel06@students.lcc.lt>
2021-07-11 21:38:19 +0300

    task12

--- day3.py
+++ day3.py
@@ -51,10 +51,28 @@ def divisibleByFive():
     for number in numbers:
         if int(number, 2) % 5 == 0:
             divisibleFive.append(number)
-        elif int(number, 2) == 0:
-            pass
 
     print(", ".join(divisibleFive))
 
 
 divisibleByFive()
+
+"""
+Question 12:
+Write a program, which will find all such numbers between 1000 and 3000 (both included) such that each digit of the
+number is an even number.The numbers obtained should be printed in a comma-separated sequence on a single line.
+
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input.
+"""
+
+def foundEven():
+    evenNumbers = []
+    for number in range(1000, 3001):
+        if int(number) % 2 == 0:
+            evenNumbers.append(str(number))
+
+    print(", ".join(evenNumbers))
+
+
+foundEven()
