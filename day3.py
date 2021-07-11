183d454d911382011578dab2ed8060483b8e454b
Vladas Verkelis <vverkel06@students.lcc.lt>
2021-07-11 21:07:33 +0300

    task11

--- day3.py
+++ day3.py
@@ -26,3 +26,35 @@ def someFunction():
 
 
 someFunction()
+
+"""
+Question 11:
+Write a program which accepts a sequence of comma separated 4 digit binary numbers as its input and then check whether
+they are divisible by 5 or not. The numbers that are divisible by 5 are to be printed in a comma separated sequence.
+
+Example:
+
+0100,0011,1010,1001
+Then the output should be:
+
+1010
+Notes: Assume the data is input by console.
+
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input.
+"""
+
+def divisibleByFive():
+    divisibleFive = []
+    numbers = input("Please, enter 4 digits binary numbers, separated by comma: ")
+    numbers = numbers.split(",")
+    for number in numbers:
+        if int(number, 2) % 5 == 0:
+            divisibleFive.append(number)
+        elif int(number, 2) == 0:
+            pass
+
+    print(", ".join(divisibleFive))
+
+
+divisibleByFive()
