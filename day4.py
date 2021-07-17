51a45cd96ab9d78941dac67c7c1a5f616f3b20c9
Vladas Verkelis <vverkel06@students.lcc.lt>
2021-07-17 15:26:45 +0300

    task15

--- day4.py
+++ day4.py
@@ -28,3 +28,36 @@ def countsUpperLower():
 
 
 countsUpperLower()
+
+"""
+Question 15:
+Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a.
+
+Suppose the following input is supplied to the program:
+
+9
+Then, the output should be:
+
+11106
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input.
+"""
+
+def someCalculations():
+    while True:
+        try:
+            number = int(input("Please, enter some number: "))
+            number = str(number)
+            secondNumber = number + number
+            thirdNumber = number + number + number
+            fourthNumber = number + number + number + number
+            firstNumber = int(number)
+            secondNumber = int(secondNumber)
+            thirdNumber = int(thirdNumber)
+            fourthNumber = int(fourthNumber)
+            print(firstNumber + secondNumber + thirdNumber + fourthNumber)
+        except:
+            print("You should enter some number.")
+
+
+someCalculations()
