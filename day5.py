20c49842b7d019923c6f33db48d6fefe88d62253
Vladas Verkelis <vverkel06@students.lcc.lt>
2021-07-19 23:22:10 +0300

    task17

--- day5.py
+++ day5.py
@@ -24,3 +24,41 @@ def squareEachOddNumber():
 
 
 squareEachOddNumber()
+
+"""
+Question 17:
+Write a program that computes the net amount of a bank account based a transaction log from console input. 
+The transaction log format is shown as following:
+
+D 100
+W 200
+D means deposit while W means withdrawal.
+Suppose the following input is supplied to the program:
+
+D 300
+D 300
+W 200
+D 100
+Then, the output should be:
+
+500
+Hints:
+In case of input data being supplied to the question, it should be assumed to be a console input.
+"""
+
+def calculateBalance():
+    balance = 0
+    while True:
+        transaction = str(input("Please, enter 'D' for deposit, 'W' for withdrawal and the sum or 'Q' to stop: "))
+        if transaction[0].upper() == 'D':
+            sum = int(transaction[1:])
+            balance += sum
+        elif transaction[0].upper() == 'W':
+            sum = int(transaction[1:])
+            balance -= sum
+        elif transaction.upper() == 'Q':
+            break
+    print(balance)
+
+
+calculateBalance()
