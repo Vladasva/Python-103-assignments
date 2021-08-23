38893b364d2d6dddc9b11096bead6393b6af4eaa
Vladasva <vladasverkelis@yahoo.com>
2021-08-23 23:14:51 +0300

    task27

--- day9.py
+++ day9.py
@@ -11,3 +11,20 @@ def sumOfTwoNumber(number1, number2):
 
 
 sumOfTwoNumber(30, 45)
+
+"""
+Question 27:
+Define a function that can convert a integer into a string and print it in console.
+
+Hints:
+Use str() to convert a number to string.
+"""
+
+def convertItnToString(number):
+    numberString = str(number)
+    return print(numberString)
+
+
+convertItnToString(5)
+
+
