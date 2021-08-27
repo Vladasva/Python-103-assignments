2eb7cdfdec7ed72714e21e6c4cd141aabaa6cd37
Vladasva <vladasverkelis@yahoo.com>
2021-08-27 23:04:09 +0300

    task30

--- day9.py
+++ day9.py
@@ -50,5 +50,25 @@ Use + sign to concatenate the strings.
 concatenateStrings = lambda s1, s2: s1 + s2
 print(concatenateStrings("5", "3"))
 
+"""
+Question 30:
+Define a function that can accept two strings as input and print the string with maximum length in console. If two
+strings have the same length, then the function should print all strings line by line.
+
+Hints:
+Use len() function to get the length of a string.
+"""
+
+def checkStringLength(string1, string2):
+    len1 = len(string1)
+    len2 = len(string2)
+    if len1 == len2:
+        print(string1)
+        print(string2)
+    elif len1 > len2:
+        print(string1)
+    elif len1 < len2:
+        print(string2)
 
 
+checkStringLength("Yes", "No")
