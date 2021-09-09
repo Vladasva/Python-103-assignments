232953af302cfeada04787b35e911aef6a58da89
Vladasva <vladasverkelis@yahoo.com>
2021-09-09 22:00:09 +0300

    task33

--- day10.py
+++ day10.py
@@ -8,16 +8,16 @@ Use dict[key]=value pattern to put entry into a dictionary.Use ** operator to ge
 loops.
 """
 
-def squareDictionaryOneToTwenty():
-    squaredOneToTwenty = {}
-    for _ in range(1, 20+1):
-        if _ > 0:
-            squaredOneToTwenty[_] = (_ ** 2)
-
-    print(squaredOneToTwenty)
-
-
-squareDictionaryOneToTwenty()
+# def squareDictionaryOneToTwenty():
+#     squaredOneToTwenty = {}
+#     for _ in range(1, 20+1):
+#         if _ > 0:
+#             squaredOneToTwenty[_] = (_ ** 2)
+#
+#     print(squaredOneToTwenty)
+#
+#
+# squareDictionaryOneToTwenty()
 
 """
 Question 32:
@@ -29,14 +29,31 @@ Use dict[key]=value pattern to put entry into a dictionary.Use ** operator to ge
 loops.Use keys() to iterate keys in the dictionary. Also we can use item() to get key/value pairs.
 """
 
-def squareDictionaryOneToTwenty():
-    squaredOneToTwenty = {}
-    for _ in range(1, 20+1):
-        if _ > 0:
-            squaredOneToTwenty[_] = (_ ** 2)
+# def squareDictionaryOneToTwenty():
+#     squaredOneToTwenty = {}
+#     for _ in range(1, 20+1):
+#         if _ > 0:
+#             squaredOneToTwenty[_] = (_ ** 2)
+#
+#     print(squaredOneToTwenty.keys())
+#
+#
+# squareDictionaryOneToTwenty()
 
-    print(squaredOneToTwenty.keys())
+"""
+Question 33:
+Define a function which can generate and print a list where the values are square of numbers between 1 and 20 (both
+included).
 
+Hints:
+Use ** operator to get power of a number.Use range() for loops.Use list.append() to add values into a list.
+"""
 
-squareDictionaryOneToTwenty()
+def squareListOneTwoTwenty():
+    squaredListOneTwoTwenty = []
+    for _ in range(1, 21):
+        squaredListOneTwoTwenty.append(_ ** 2)
 
+    print(squaredListOneTwoTwenty)
+
+squareListOneTwoTwenty()
