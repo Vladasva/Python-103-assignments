f1b0bc756db6861e38d71f9c0c962793b5eb3da9
Vladasva <vladasverkelis@yahoo.com>
2021-09-13 23:52:34 +0300

    task36

--- day10.py
+++ day10.py
@@ -96,3 +96,22 @@ def squareListOneTwoTwenty():
 
 
 squareListOneTwoTwenty()
+
+"""
+Question:
+Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included).
+Then the function needs to print all values except the first 5 elements in the list.
+
+Hints: Use ** operator to get power of a number.Use range() for loops.Use list.append() to add values into a list.
+Use [n1:n2] to slice a list
+"""
+
+def squareListOneTwoTwenty():
+    squaredListOneTwoTwenty = []
+    for _ in range(1, 21):
+        squaredListOneTwoTwenty.append(_ ** 2)
+
+    print(squaredListOneTwoTwenty[6:21])
+
+
+squareListOneTwoTwenty()
