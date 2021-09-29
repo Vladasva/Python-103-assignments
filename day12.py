28febb0af200bd34c0c01e7e1d4055f835b6abfa
Vladasva <vladasverkelis@yahoo.com>
2021-09-29 08:32:40 +0300

    task44

---
+++ day12.py
@@ -0,0 +1,13 @@
+"""
+Question 44:
+Write a program which can map() to make a list whose elements are square of numbers between 1 and 20 (both included).
+
+Hints:
+Use map() to generate a list. Use lambda to define anonymous functions.
+"""
+
+def sqr(x):
+    return x*x
+
+squaredNumbers = list(map(sqr, range(1, 21)))
+print(squaredNumbers)
