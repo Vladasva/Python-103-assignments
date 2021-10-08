50e4e9e16ffcf3ed6a0322964c8bc4f10048db94
Vladasva <vladasverkelis@yahoo.com>
2021-10-08 23:57:09 +0300

    task48

--- day13.py
+++ day13.py
@@ -17,3 +17,24 @@ class Circle:
 
 circle = Circle(5)
 print(circle.area())
+
+"""
+Question 48:
+Define a class named Rectangle which can be constructed by a length and width. The Rectangle class has a method which 
+can compute the area.
+
+Hints
+Use def methodName(self) to define a method.
+"""
+
+class Rectangle():
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+
+
+rectangle = Rectangle(4, 5)
+print(rectangle.area())
