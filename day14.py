087dcbb8edcb5474342f8722bc9be3d8617e8d6e
Vladasva <vladasverkelis@yahoo.com>
2021-10-30 19:04:52 +0300

    task52

--- day14.py
+++ day14.py
@@ -15,5 +15,34 @@ def computeZeroFromFive():
 
 computeZeroFromFive()
 
+"""
+Question 52:
+Define a custom exception class which takes a string message as attribute.
+
+Hints
+To define a custom exception, we need to define a class inherited from Exception.
+"""
+
+class CustomException(Exception):
+    """Exception raised for custom purpose
+
+    Attributes:
+        message -- explanation of the error
+    """
+
+    def __init__(self, message):
+        self.message = message
+
+
+num = int(input())
+
+try:
+    if num < 10:
+        raise CustomException("Input is less than 10")
+    elif num > 10:
+        raise CustomException("Input is grater than 10")
+except CustomException as ce:
+    print("The error raised: " + ce.message)
+
 
 
