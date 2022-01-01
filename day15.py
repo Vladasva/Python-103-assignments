dc70a12e71b4a21715a5fbdc494185582e5ac6a5
Vladasva <vladasverkelis@yahoo.com>
2022-01-01 21:47:23 +0200

    task58

--- day15.py
+++ day15.py
@@ -76,3 +76,12 @@ Use unicode()/encode() function to convert.
 s = input()
 u = s.encode('utf-8')
 print(u)
+
+"""
+Question 58:
+Write a special comment to indicate a Python source code file is in unicode.
+
+Hints
+Use unicode() function to convert.
+"""
+# -*- coding: utf-8 -*-
